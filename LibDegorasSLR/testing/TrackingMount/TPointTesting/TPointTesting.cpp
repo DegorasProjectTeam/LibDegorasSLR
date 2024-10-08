9d32d2f9e8bda9615ee9aa6ce3dcbd405eafa74d
Jesús Relinque Madroñal <jesus.relinquema@gmail.com>
2024-10-08 14:48:05 +0200

    Changed TPoint plotting and test calculating.

--- LibDegorasSLR/testing/TrackingMount/TPointTesting/TPointTesting.cpp
+++ LibDegorasSLR/testing/TrackingMount/TPointTesting/TPointTesting.cpp
@@ -4,86 +4,99 @@
 #include <omp.h>
 #include <cmath>
 #include <fstream>
+#include <filesystem>
 
 int main()
 {
-
-    std::string path("inputs/model.dat");
-
-    dpslr::mount::models::TPointCoefficients coefs;
-    auto error = dpslr::mount::models::parseTPointModelFile(path, coefs);
-
-    if (error != dpslr::mount::models::TPointParseError::NOT_ERROR || coefs.empty())
-    {
-        std::cerr << "Cannot load TPoint terms from file in path " << path << std::endl;
-        return -1;
-    }
-
-    std::cout << "Loaded model is: " << std::endl;
-    for (const auto& parallel_group : coefs)
-    {
-        std::cout << "(";
-        for (auto coef_it = parallel_group.begin(); coef_it != parallel_group.end(); coef_it++)
-        {
-            std::cout << coef_it->name << " = " << coef_it->value;
-            if (coef_it + 1 != parallel_group.end())
-                std::cout << ", ";
-        }
-        std::cout << ") ";
-    }
-
-    std::cout << std::endl;
-
-    unsigned factor = 1;
+    unsigned factor = 10;
     unsigned min_elev = 10, max_elev = 85;
-    unsigned col_size = (max_elev - min_elev) * factor;
 
-    const std::vector<dpslr::astro::types::AltAzPos> zero_pos_vector(col_size, {0., 0.});
-    const std::vector<double> zero_vector(col_size, 0.);
-    std::vector<std::vector<dpslr::astro::types::AltAzPos>> modified_positions(36000, zero_pos_vector);
-    std::vector<std::vector<double>> error_az(36000, zero_vector);
-    std::vector<std::vector<double>> error_el(36000, zero_vector);
-    std::vector<std::vector<double>> error_rms(36000, zero_vector);
 
-    omp_set_num_threads(8);
-
-    #pragma omp parallel for
-    for (unsigned i = 0; i < 360 * factor; i++)
+    if (!std::filesystem::exists("errors.csv"))
     {
-        for (unsigned j = min_elev * factor; j < max_elev * factor; j++)
+        std::string path("inputs/model.dat");
+
+        dpslr::mount::models::TPointCoefficients coefs;
+        auto error = dpslr::mount::models::parseTPointModelFile(path, coefs);
+
+        if (error != dpslr::mount::models::TPointParseError::NOT_ERROR || coefs.empty())
         {
-            dpslr::astro::types::AltAzPos p(i / (1. * factor), j / (1. * factor));
-            unsigned col_idx = j - min_elev * factor;
-            modified_positions[i][col_idx] = dpslr::mount::models::computeCorrectedByTPointPosition(coefs, p);
-            error_az[i][col_idx] = p.az - modified_positions[i][col_idx].az;
-            error_el[i][col_idx] = p.el - modified_positions[i][col_idx].el;
-            error_rms[i][col_idx] = std::sqrt((error_az[i][col_idx]*error_az[i][col_idx] +
-                                               error_el[i][col_idx]*error_el[i][col_idx]) / 2.);
+            std::cerr << "Cannot load TPoint terms from file in path " << path << std::endl;
+            return -1;
         }
+
+        std::cout << "Loaded model is: " << std::endl;
+        for (const auto& parallel_group : coefs)
+        {
+            std::cout << "(";
+            for (auto coef_it = parallel_group.begin(); coef_it != parallel_group.end(); coef_it++)
+            {
+                std::cout << coef_it->name << " = " << coef_it->value;
+                if (coef_it + 1 != parallel_group.end())
+                    std::cout << ", ";
+            }
+            std::cout << ") ";
+        }
+
+        std::cout << std::endl;
+        unsigned col_size = (max_elev - min_elev) * factor;
+
+        const std::vector<dpslr::astro::types::AltAzPos> zero_pos_vector(col_size, {0.L, 0.L});
+        const std::vector<double> zero_vector(col_size, 0.);
+        std::vector<std::vector<dpslr::astro::types::AltAzPos>> modified_positions(36000, zero_pos_vector);
+        std::vector<std::vector<double>> error_az(36000, zero_vector);
+        std::vector<std::vector<double>> error_el(36000, zero_vector);
+        std::vector<std::vector<double>> error_rms(36000, zero_vector);
+
+        omp_set_num_threads(8);
+
+        #pragma omp parallel for
+        for (unsigned i = 0; i < 360 * factor; i++)
+        {
+            for (unsigned j = min_elev * factor; j < max_elev * factor; j++)
+            {
+                dpslr::astro::types::AltAzPos p(i / (1.L * factor), j / (1.L * factor));
+                unsigned col_idx = j - min_elev * factor;
+                modified_positions[i][col_idx] = dpslr::mount::models::computeCorrectedByTPointPosition(coefs, p);
+                error_az[i][col_idx] = p.az - modified_positions[i][col_idx].az;
+                error_el[i][col_idx] = p.el - modified_positions[i][col_idx].el;
+                error_rms[i][col_idx] = std::sqrt((error_az[i][col_idx]*error_az[i][col_idx] +
+                                                   error_el[i][col_idx]*error_el[i][col_idx]) / 2.);
+            }
+        }
+
+
+        std::ofstream out("errors.csv");
+
+        out << "az,el,az_error,el_error,rms" << std::endl;
+
+        out << std::setprecision(8);
+
+        for (unsigned i = 0; i < 360 * factor; i++)
+        {
+            for (unsigned j = min_elev * factor; j < max_elev * factor; j++)
+            {
+
+                unsigned col_idx = j - min_elev * factor;
+                out << i / static_cast<double>(factor) << "," << j / static_cast<double>(factor) << ","
+                    << error_az[i][col_idx] << "," << error_el[i][col_idx] << "," <<  error_rms[i][col_idx];
+                out << std::endl;
+            }
+        }
+
     }
-
-    std::ofstream out("errors.csv");
-
-    out << std::setprecision(8);
-
-    for (unsigned i = 0; i < 360 * factor; i++)
+    else
     {
-        for (unsigned j = min_elev * factor; j < max_elev * factor; j++)
-        {
-
-            unsigned col_idx = j - min_elev * factor;
-            out << i / static_cast<double>(factor) << "," << j / static_cast<double>(factor) << ","
-                << error_az[i][col_idx] << "," << error_el[i][col_idx] << "," <<  error_rms[i][col_idx];
-            out << std::endl;
-        }
+        std::cout << "The file errors.csv already exists. Only plotting. If you want to recalculate, delete errors.csv"
+                  << std::endl;
     }
-
 
     std::string current_dir = dpslr::helpers::files::getCurrentDir();
 
     // Configure the python script executable.
     std::string python_plot_script(current_dir+"/python_scripts/plot.py");
-    std::string python_cmd = "python \"" + python_plot_script + "\" ";
+    std::string python_cmd = "python \"" + python_plot_script + "\" " +
+                             std::to_string(factor) + " " + std::to_string(min_elev) + " " + std::to_string(max_elev);
 
     std::cout<<"Plotting data using Python helpers..."<<std::endl;
     if(system(std::string(python_cmd).c_str()))
--- LibDegorasSLR/testing/TrackingMount/TPointTesting/python_scripts/plot.py
+++ LibDegorasSLR/testing/TrackingMount/TPointTesting/python_scripts/plot.py
@@ -1,91 +1,79 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
 
-This is a temporary script file.
-"""
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 from scipy.interpolate import griddata
 from matplotlib.colors import Normalize
-
-df = pd.read_csv('errors.csv', header=None)
-df.columns = ['az', 'el', 'az_error', 'el_error', 'rms']
-
-az = np.array(df['az'])
-el = np.array(df['el'])
-az_error = np.array(df['az_error'])
-el_error = np.array(df['el_error'])
-rms = np.array(df['rms'])
-
-factor = 1
-min_elev = 10
-max_elev = 85
-rows = 360 * factor
-cols = (max_elev - min_elev) * factor
-
-az = az.reshape(rows, cols)
-el = el.reshape(rows, cols)
-az_error = az_error.reshape(rows, cols)
-el_error = el_error.reshape(rows, cols)
-rms = rms.reshape(rows, cols)
-
-fig1, ax1 = plt.subplots(subplot_kw={'projection': 'polar'})
-
-ax1.set_theta_zero_location('N')
-ax1.set_theta_direction(-1)
-ax1.set_yticks(np.arange(0, 91, 20))
-ax1.set_yticklabels(np.arange(0, 91, 20))
-ax1.set_title('Distribución de Errores del eje de Azimuth')
-
-vmin1 = -0.045
-vmax1 = 0.05
-norm1 = Normalize(vmin=vmin1, vmax=vmax1)
-
-c = ax1.pcolormesh(az, el, az_error, norm=norm1)
-# set the limits of the plot to the limits of the data
-fig1.colorbar(c, ax=ax1)
-ax1.grid()
+from matplotlib import cm
+import sys
 
 
-fig2, ax2 = plt.subplots(subplot_kw={'projection': 'polar'})
+if __name__ == "__main__":
 
-ax2.set_theta_direction(-1)
-ax2.set_yticks(np.arange(0, 91, 20))
-ax2.set_theta_zero_location('N')
-ax2.set_yticklabels(np.arange(0, 91, 20))
-ax2.set_title('Distribución de Errores del eje de Elevación')
+    # Get external file by arguments. If no external file, use the manually selected.
+    try:
+        factor = int(sys.argv[1])
+    except (ValueError, IndexError):
+        factor = 1
 
-el_min, el_max = 0.001, 0.1
+    try:
+        min_elev = int(sys.argv[2])
+    except (ValueError, IndexError):
+        min_elev = 0
 
-vmin2 = -0.045
-vmax2 = 0.05
-norm2 = Normalize(vmin=vmin2, vmax=vmax2)
+    try:
+        max_elev = int(sys.argv[3])
+    except (ValueError, IndexError):
+        max_elev = 90
 
-c = ax2.pcolormesh(az, el, el_error, norm=norm2)
-# set the limits of the plot to the limits of the data
-fig2.colorbar(c, ax=ax2)
-ax2.grid()
+    df = pd.read_csv('errors.csv', header=0)
+    df.columns = ['az', 'el', 'az_error', 'el_error', 'rms']
+
+    az = np.array(df['az'])
+    el = np.array(df['el'])
+    az_error = np.array(df['az_error'])
+    el_error = np.array(df['el_error'])
+    rms = np.array(df['rms'])
+
+    rows = 360 * factor
+    cols = (max_elev - min_elev) * factor
+
+    az = az.reshape(rows, cols)
+    el = el.reshape(rows, cols)
+    az_error = az_error.reshape(rows, cols)
+    el_error = el_error.reshape(rows, cols)
+    rms = rms.reshape(rows, cols)
+
+    fig1, ax1 = plt.subplots(subplot_kw={'projection': '3d'})
+
+    ax1.set_title('Distribución de Errores del eje de Azimuth')
+
+    c = ax1.plot_surface(az, el, az_error, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    # set the limits of the plot to the limits of the data
+    fig1.colorbar(c, ax=ax1)
+    ax1.grid()
 
 
-fig3, ax3 = plt.subplots(subplot_kw={'projection': 'polar'})
+    fig2, ax2 = plt.subplots(subplot_kw={'projection': '3d'})
 
-ax3.set_theta_direction(-1)
-ax3.set_yticks(np.arange(0, 91, 20))
-ax3.set_theta_zero_location('N')
-ax3.set_yticklabels(np.arange(0, 91, 20))
-ax3.set_title('Distribución de RMS')
+    ax2.set_title('Distribución de Errores del eje de Elevación')
 
-vmin3 = 0.
-vmax3 = 0.01
-norm3 = Normalize(vmin=vmin3, vmax=vmax3)
-
-c = ax3.pcolormesh(az, el, rms, norm=norm3)
-# set the limits of the plot to the limits of the data
-fig3.colorbar(c, ax=ax3)
-ax3.grid()
+    c = ax2.plot_surface(az, el, el_error, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    # set the limits of the plot to the limits of the data
+    fig2.colorbar(c, ax=ax2)
+    ax2.grid()
 
 
-plt.show()
+    fig3, ax3 = plt.subplots(subplot_kw={'projection': '3d'})
+
+    ax3.set_title('Distribución de RMS')
+
+    c = ax3.plot_surface(az, el, rms, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    # set the limits of the plot to the limits of the data
+    fig3.colorbar(c, ax=ax3)
+    ax3.grid()
+
+
+    plt.show()
